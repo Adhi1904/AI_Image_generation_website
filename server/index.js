dc4bfa31baff9cfd9212f48d5420273ca92dd1ba
Harshith M <harshith.m@learner.manipal.edu>
2023-05-01 19:50:25 +0530

    done

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import  express  from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import  connectDB  from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+
+const app=express();
+app.use(cors());
+
+app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get("/",async (req, res) => {
+    res.send("Hello World");
+});
+
+const startServer = async () => {
+
+    try{
+        connectDB(process.env.MONGODB_URL)
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}` 
+            );
+        });
+
+    }
+    catch(err){
+        console.log(err);
+    }
+
+    
+}
+
+startServer();
